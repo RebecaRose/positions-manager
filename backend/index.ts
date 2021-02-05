43d655afb2edf0d84dcf9974df35aa7ceb45e7ef
rebecarose <rebecarose21@gmail.com>
2021-02-04 21:17:04 -0300

    implements users crud

---
+++ backend/index.ts
@@ -0,0 +1,12 @@
+const express = require("express");
+const db = require("./database.ts");
+const app = express();
+
+const userRoute = require('./routes/users.ts');
+
+app.use(express.json());
+app.use('/', userRoute);
+
+const PORT = process.env.PORT || 3500;
+
+app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
